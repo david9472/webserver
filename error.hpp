ed7fc6e7fa2a32be6c58db306e228f06eebc62d2
David, Fruehwirth <david.fruehwirth@student.tugraz.at>
2024-07-12 12:27:20 +0200

    Added new exception type

---
+++ error.hpp
@@ -0,0 +1,38 @@
+//
+// Created by david on 12/07/24.
+//
+
+#ifndef WEBSERVER_ERROR_HPP
+#define WEBSERVER_ERROR_HPP
+
+#include "logger.hpp"
+
+#include<fmt/core.h>
+
+#include <stdexcept>
+
+namespace logging
+{
+  class Error : public std::exception
+  {
+  private:
+    std::string error_;
+    std::string full_error_;
+  public:
+    Error(const std::string &fileName, const std::string &functionName, const long lineNumber,
+          const std::string &errorMsg) : error_(errorMsg)
+    {
+      full_error_ = fmt::format("{} in File: {} Function: {} Line: {}", errorMsg, fileName, functionName, lineNumber);
+      Logger::getInstance().log(LogLevel::ERROR, full_error_);
+    }
+
+    [[nodiscard]] const char *what() const noexcept override
+    {
+      return error_.c_str();
+    }
+  };
+
+  #define LOC __FILE__, __func__, __LINE__
+}
+
+#endif //WEBSERVER_ERROR_HPP
